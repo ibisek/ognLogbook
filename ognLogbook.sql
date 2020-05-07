418069297582f99863fe5b27c52c5cb776bc19f3
ibisek <ibisek@protonmail.com>
2020-05-07 15:23:16 +0200

    DDB registrations

--- ognLogbook.sql
+++ ognLogbook.sql
@@ -12,12 +12,29 @@ CREATE TABLE logbook_events (
     event VARCHAR(1),
     lat DECIMAL(8,5),
     lon DECIMAL(8,5),
-    location_icao VARCHAR(4) DEFAULT 'tbd',
+    location_icao VARCHAR(4) DEFAULT 'tbd'
     --location_name VARCHAR(30)
-    aircraft_registration VARCHAR(4) DEFAULT 'tbd',
-    aircraft_cn VARCHAR(3)   
+);
+
+--DROP TABLE IF EXISTS ddb;
+CREATE TABLE ddb (
+	id BIGINT PRIMARY KEY auto_increment,
+	device_type VARCHAR(1),
+	device_id VARCHAR(6),
+	aircraft_type VARCHAR(32),
+	aircraft_registration VARCHAR(4) DEFAULT 'tbd',
+	aircraft_cn VARCHAR(3),
+	tracked BOOL,
+	identified BOOL
 );
 
 --
 
-SELECT * FROM logbook_events;
+SELECT count(id) FROM logbook_events;
+
+SELECT l.*, d.* FROM logbook_events as l JOIN ddb as d ON l.address = d.device_id;
+
+select count(id) as n , address from logbook_events group by address order by n desc;
+
+SELECT * FROM logbook_events where address='DD8220';
+
