76d498be40ae6a3f7f1a7313ffe69ed7010d0ac2
ibisek <ibisek@protonmail.com>
2020-05-07 11:55:20 +0200

    db structure

---
+++ ognLogbook.sql
@@ -0,0 +1,21 @@
+
+--create database ogn-logbook;
+--grant all on ogn_logbook.* to '**'@'localhost' identified by '**';
+
+--DROP TABLE IF EXISTS logbook_events ;
+CREATE TABLE logbook_events (
+    id BIGINT PRIMARY KEY auto_increment,
+    ts BIGINT,
+    address VARCHAR(6),
+    address_type TINYINT,
+    aircraft_type TINYINT,
+    event VARCHAR(1),
+    lat DECIMAL(8,5),
+    lon DECIMAL(8,5),
+    location_icao VARCHAR(4)
+    --location_name VARCHAR(30)
+);
+
+--
+
+SELECT * FROM logbook_events;
