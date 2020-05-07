3306dc0172fa8859f7d26430143166417c499b79
ibisek <ibisek@protonmail.com>
2020-05-07 18:12:46 +0200

    airfield icao code resolution

---
+++ src/airfieldManager.py
@@ -0,0 +1,80 @@
+
+import json
+import math
+
+from singleton import Singleton
+
+
+class AirfieldRecord(object):
+
+    def __init__(self, map: dict):
+        self.lat = math.radians(map['lat'])
+        self.lon = math.radians(map['lon'])
+        self.code = map['code']
+
+
+class AirfieldManager(object, metaclass=Singleton):
+
+    def __init__(self):
+        self.airfields = []
+
+        with open('../data/airfields.json', 'r') as f:
+            j = json.load(f)
+            for item in j:
+                ar = AirfieldRecord(item)
+                self.airfields.append(ar)
+
+        print(f"[INFO] num airfields: {len(self.airfields)}")
+
+    @staticmethod
+    def getDistanceInKm(lat1: float, lon1: float, lat2: float, lon2: float):
+        """
+        :param lat1: in radians (!)
+        :param lon1: in radians (!)
+        :param lat2: in radians (!)
+        :param lon2: in radians (!)
+        :return: ICAO code of the nearest airfield
+        """
+        arg = math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2) * math.cos(lon2 - lon1)
+        if arg >= 1.0:
+            return 0
+
+        R = 6371  # km
+        dist = math.acos(arg) * R
+
+        return dist
+
+    def getNearest(self, lat, lon):
+        minDist = 99999999999999
+        code = None
+
+        latRad = math.radians(lat)
+        lonRad = math.radians(lon)
+
+        for rec in self.airfields:
+            dist = AirfieldManager.getDistanceInKm(latRad, lonRad, rec.lat, rec.lon)
+            if dist < minDist:
+                minDist = dist
+                code = rec.code
+
+        if minDist < 4:   # [km]
+            return code
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    am = AirfieldManager()
+
+    lat = 49.16
+    lon = 16.11
+
+    lat = 49.3697147
+    lon = 16.1141575
+
+    # lat = 50.32798
+    # lon = 15.95643
+
+    icao = am.getNearest(lat, lon)
+    print(icao)
+
--- src/beaconProcessor.py
+++ src/beaconProcessor.py
@@ -8,12 +8,13 @@ from rq import Queue
 from configuration import SPEED_THRESHOLD, redisConfig, dbConnectionInfo, REDIS_RECORD_EXPIRATION
 from db.DbThread import DbThread
 from db.DbSource import DbSource
+from airfieldManager import AirfieldManager
 
 
 class BeaconProcessor(object):
 
     redis = StrictRedis(**redisConfig)
-    queue = Queue('beacons', is_async=True, connection=redis)
+    queue = Queue('beacons', is_async=False, connection=redis)
 
     def __init__(self):
         # self.dbThread = DbThread(dbConnectionInfo)
@@ -27,9 +28,13 @@ class BeaconProcessor(object):
         if beacon['beacon_type'] != 'aprs_aircraft':
             return
 
+        # we are not interested in para, baloons, uavs, static stuff and others:
+        if beacon['aircraft_type'] in [4, 6, 7, 13, 11, 15, 16]:
+            return
+
         address = beacon['address']
         groundSpeed = beacon['ground_speed']
-        print(f"[INFO] {address} gs: {groundSpeed:.0f}")
+        # print(f"[INFO] {address} gs: {groundSpeed:.0f}")
 
         currentStatus = 0 if groundSpeed < SPEED_THRESHOLD else 1    # 0 = on ground, 1 = airborne, -1 = unknown
         # TODO add AGL check (?)
@@ -52,6 +57,10 @@ class BeaconProcessor(object):
             lat = beacon['latitude']
             lon = beacon['longitude']
 
+            icaoLocation = AirfieldManager().getNearest(lat, lon)
+            if not icaoLocation:
+                return
+
             event = 'L' if currentStatus == 0 else 'T'  # L = landing, T = take-off
 
             # strSql = f"INSERT INTO logbook_events (ts, address, address_type, aircraft_type, event, lat, lon) " \
@@ -66,16 +75,18 @@ class BeaconProcessor(object):
             # data['lat'] = float(f"{lat:.5f}")
             # data['lon'] = float(f"{lon:.5f}")
 
-            strSql = f"INSERT INTO logbook_events (ts, address, address_type, aircraft_type, event, lat, lon) " \
-                     f"VALUES (%s, %s, %s, %s, %s, %s, %s);"
+            strSql = f"INSERT INTO logbook_events " \
+                     f"(ts, address, address_type, aircraft_type, event, lat, lon, location_icao) " \
+                     f"VALUES " \
+                     f"(%s, %s, %s, %s, %s, %s, %s, %s);"
 
-            data = (ts, address, addressType, aircraftType, event, float(f"{lat:.5f}"), float(f"{lon:.5f}"))
+            data = (ts, address, addressType, aircraftType, event, float(f"{lat:.5f}"), float(f"{lon:.5f}"), icaoLocation)
 
             with DbSource(dbConnectionInfo).getConnection() as cur:
-                # query = cur.mogrify(strSql, data)
-                # print('query:', query)
+                query = cur.mogrify(strSql, data)
+                print('query:', query)
                 # self.dbThread.addStatement(query)
-                cur.execute(strSql, data)
+                # cur.execute(strSql, data)
 
     startTime = time.time()
     numEnquedTasks = 0
