9f25a4246c978fe699c0f497ca08f37918325fb2
ibisek <ibisek@protonmail.com>
2020-05-10 10:47:43 +0200

    more constants into configuration

--- src/configuration.py
+++ src/configuration.py
@@ -1,8 +1,13 @@
 
+APRS_FILTER = 'r/+49.3678/+16.1145/250'
+# APRS_FILTER = None
+
 SPEED_THRESHOLD = 40    # [km/h]
 
 redisConfig = {"host": "127.0.0.1", "password": "", "port": 6379}
 REDIS_RECORD_EXPIRATION = 12*60*60     # [s]
+
+NUM_RAW_WORKERS = 2
 
 DB_URL = '127.0.0.1'
 DB_PORT = 3306
