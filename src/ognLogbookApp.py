c52c10813e68707e8ce2a50e3ccdbcb8c9155720
ibisek <ibisek@protonmail.com>
2020-05-10 10:49:29 +0200

    correct shutdown hell

--- src/ognLogbookApp.py
+++ src/ognLogbookApp.py
@@ -1,10 +1,16 @@
 
+import sys
+import signal
+import time
 
 from ogn.client import AprsClient
 
+from configuration import APRS_FILTER
 from beaconProcessor import BeaconProcessor
 
 bp = BeaconProcessor()
+doRun = True
+client: AprsClient = AprsClient(aprs_user='ibisek', aprs_filter=APRS_FILTER)
 
 
 def process_beacon(raw_message):
@@ -17,20 +23,41 @@ def process_beacon(raw_message):
     bp.enqueueForProcessing(raw_message)
 
 
+def signal_handler(sig, frame):
+    print('SIGINT / Ctrl+C detected!')
+
+    bp.stop()
+
+    global doRun
+    doRun = False
+
+    if client:
+        print("XX killing the client")
+        client.disconnect()
+        client.sock.shutdown(0)     # force the client to stop!
+        client.sock.close()         # force the client to stop!
+
+    print('[INFO] Shutdown initiated.')
+    time.sleep(4000)
+    sys.exit(1)
+    quit(-1)
+
+
 if __name__ == '__main__':
-    aprsFilter = 'r/+49.3678/+16.1145/250'
-    # aprsFilter = None
 
-    client = AprsClient(aprs_user='ibisek', aprs_filter=aprsFilter)
-    client.connect()
+    signal.signal(signal.SIGINT, signal_handler)
 
-    doRun = True
     while doRun:
         try:
+            client.connect()
             client.run(callback=process_beacon, autoreconnect=True)
+            print("XX CLIENT 2")
         except KeyboardInterrupt:
             print('\nApp interrupted.')
             client.disconnect()
             doRun = False
         except TimeoutError as ex:
             print("ERROR:", str(ex))
+
+    print('[INFO] Quitting the APP.')
+    sys.exit(0)
