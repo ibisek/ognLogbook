93831c9bab1fed5e0e1a67035f437edb5afbc81a
ibisek <ibisek@protonmail.com>
2020-05-14 20:05:28 +0200

    ICAO code can be longer than 4 chars..

--- ognLogbook.sql
+++ ognLogbook.sql
@@ -12,7 +12,7 @@ CREATE TABLE logbook_events (
     event VARCHAR(1),
     lat DECIMAL(8,5),
     lon DECIMAL(8,5),
-    location_icao VARCHAR(4) DEFAULT 'tbd',
+    location_icao VARCHAR(8) DEFAULT 'tbd',
     flight_time BIGINT DEFAULT 0
 );
 
@@ -20,6 +20,19 @@ CREATE INDEX logbook_events_address ON logbook_events(address);
 CREATE INDEX logbook_events_location_icao ON logbook_events(location_icao);
 SHOW INDEXES FROM logbook_events;
 
+--DROP TABLE IF EXISTS logbook_entries;
+CREATE TABLE logbook_entries (
+  id BIGINT PRIMARY KEY auto_increment,
+  address VARCHAR(6),
+  takeoff_ts BIGINT,
+  takeoff_lat DECIMAL(8,5),
+  takeoff_lon DECIMAL(8,5),
+  takeoff_icao VARCHAR(4),
+  landing_ts BIGING,
+  landing_lat DECIMAL(8,5),
+  landing_lon DECIMAL(8,5),
+  landing_icao VARCHAR(4),
+);
 
 --DROP TABLE IF EXISTS ddb;
 CREATE TABLE ddb (
@@ -42,7 +55,24 @@ SHOW INDEXES FROM ddb;
 
 SELECT count(id) FROM logbook_events;
 
+SELECT address, count(id) as n FROM logbook_events group by address order by n desc;
+
+select * from logbook_events where address = '26A74C';
+
+--
+
 SELECT l.*, d.* FROM logbook_events as l JOIN ddb as d ON l.address = d.device_id;
+
+SELECT l.ts, l.event, l.address, l.location_icao, l.flight_time, d.aircraft_registration, d.aircraft_cn FROM logbook_events as l JOIN ddb as d ON l.address = d.device_id 
+	where location_icao = 'LKCH'
+	order by ts desc;
+
+--
+-- LANDING TIME & LOC:
+SELECT l.ts, l.address, l.location_icao, l.flight_time, d.aircraft_registration, d.aircraft_cn FROM logbook_events as l JOIN ddb as d ON l.address = d.device_id where l.event='L';
+-- TAKE OFF TIME & LOC:
+SELECT l.ts, l.address, l.location_icao FROM logbook_events as l WHERE l.address = 'DDA496' and l.event='T' and l.ts < 1589289037 ORDER BY l.ts DESC LIMIT 100;
+--
 
 select count(id) as n , address from logbook_events group by address order by n desc;
 
