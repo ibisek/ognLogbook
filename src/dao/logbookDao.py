1453330e396b47cf7f7f41bf4178e4908aff4aa9
ibisek <ibisek@protonmail.com>
2020-05-20 14:39:37 +0200

    searchable condition

--- src/dao/logbookDao.py
+++ src/dao/logbookDao.py
@@ -5,16 +5,30 @@ from db.DbSource import DbSource
 from dataStructures import LogbookItem
 
 
-def listDepartures():
+def _prepareCondition(address=None, icaoCode=None, registration=None):
+    c1 = c2 = ''
+    if icaoCode:
+        c1 = f" AND l.location_icao = '{icaoCode}'"
+    if registration:
+        c2 = f" AND d.aircraft_registration = '{registration}'"
+    cond = c1 + c2
+
+    return cond
+
+
+def listDepartures(address=None, icaoCode=None, registration=None):
+
+    cond = _prepareCondition(address=address, icaoCode=icaoCode, registration=registration)
+
     records = list()
 
     with DbSource(dbConnectionInfo).getConnection() as c:
 
-        strSql = """SELECT l.ts, l.address, l.address_type, l.aircraft_type, l.lat, l.lon, l.location_icao, 
+        strSql = f"""SELECT l.ts, l.address, l.address_type, l.aircraft_type, l.lat, l.lon, l.location_icao, 
                     d.device_type,	d.aircraft_type, d.aircraft_registration, d.aircraft_cn 
                     FROM logbook_events AS l 
                     LEFT JOIN ddb AS d ON l.address = d.device_id 
-                    WHERE l.event = 'T' AND tracked = true AND identified = true
+                    WHERE l.event = 'T' AND tracked = true AND identified = true {cond}
                     ORDER by ts desc limit 20;"""
 
         c.execute(strSql)
@@ -42,16 +56,19 @@ def listDepartures():
     return records
 
 
-def listArrivals():
+def listArrivals(address=None, icaoCode=None, registration=None):
+
+    cond = _prepareCondition(address=address, icaoCode=icaoCode, registration=registration)
+
     records = list()
 
     with DbSource(dbConnectionInfo).getConnection() as c:
 
-        strSql = """SELECT l.ts, l.address, l.address_type, l.aircraft_type, l.lat, l.lon, l.location_icao, l.flight_time, 
+        strSql = f"""SELECT l.ts, l.address, l.address_type, l.aircraft_type, l.lat, l.lon, l.location_icao, l.flight_time, 
                     d.device_type,	d.aircraft_type, d.aircraft_registration, d.aircraft_cn 
                     FROM logbook_events AS l 
                     LEFT JOIN ddb AS d ON l.address = d.device_id 
-                    WHERE l.event = 'L' AND tracked = true AND identified = true
+                    WHERE l.event = 'L' AND tracked = true AND identified = true {cond}
                     ORDER by ts desc limit 20;"""
 
         c.execute(strSql)
@@ -79,17 +96,20 @@ def listArrivals():
     return records
 
 
-def listFlights():
+def listFlights(address=None, icaoCode=None, registration=None):
+
+    cond = _prepareCondition(address=address, icaoCode=icaoCode, registration=registration)
+
     records = list()
 
     with DbSource(dbConnectionInfo).getConnection() as c:
 
-        strSql = """SELECT l.address, l.takeoff_ts, l.takeoff_lat, l.takeoff_lon, l.takeoff_icao, 
+        strSql = f"""SELECT l.address, l.takeoff_ts, l.takeoff_lat, l.takeoff_lon, l.takeoff_icao, 
                     l.landing_ts, l.landing_lat, l.landing_lon, l.landing_icao, l.flight_time,
                     d.device_type, d.aircraft_type, d.aircraft_registration, d.aircraft_cn
                     FROM logbook_entries as l 
                     LEFT JOIN ddb AS d ON l.address = d.device_id
-                    WHERE tracked = true AND identified = true
+                    WHERE tracked = true AND identified = true {cond}
                     ORDER by landing_ts desc limit 20;"""
 
         c.execute(strSql)
