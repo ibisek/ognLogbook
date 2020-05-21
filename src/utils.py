6891c09a32839be9259d5aa147859ef924ed8099
ibisek <ibisek@protonmail.com>
2020-05-21 20:15:29 +0200

    mod fix

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 
+from datetime import datetime, time
+
+
 def formatDuration(seconds):
-    h = seconds // 3600
+    h = seconds % 3600
     s = seconds - h * 3600
-    m = s // 60
+    m = s % 60
     s = s - m * 60
 
     if s > 30:
@@ -14,3 +17,18 @@ def formatDuration(seconds):
         dur = f"{m}'"
 
     return dur
+
+
+def getDayTimestamps(forDay: datetime = None):
+    """
+    :param forDay use None for today
+    :return: start, end timestamps of current day
+    """
+    if not forDay:
+        forDay = datetime.today()
+
+    startTs = datetime.combine(forDay, time.min).timestamp()
+    endTs = datetime.combine(forDay, time.max).timestamp()
+
+    return startTs, endTs
+
