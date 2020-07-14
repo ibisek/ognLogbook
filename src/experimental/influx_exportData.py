da4eb23c86a98757ddf99abb6a99f0a0b05c7fe5
ibisek <ibisek@protonmail.com>
2020-07-14 19:03:04 +0200

    memory performance optimisation

--- src/experimental/influx_exportData.py
+++ src/experimental/influx_exportData.py
@@ -10,29 +10,34 @@ if __name__ == '__main__':
 
     influx = InfluxDbThread(dbName=INFLUX_DB_NAME, host=INFLUX_DB_HOST)
 
-    # series = influx.client.get_list_series(database=INFLUX_DB_NAME)
     # measurements = influx.client.get_list_measurements()
+    series = influx.client.get_list_series(database=INFLUX_DB_NAME)
 
     with open(DUMP_FILEPATH, 'w') as f:
 
-        rs = influx.client.query(query='select * from pos')
-        for r in rs:
-            print('Num records in result set:', len(r))
-            for res in r:
-                ts = int(parser.parse(res['time']).timestamp())
-                addr = res['addr']
-                alt = res['alt']
-                gs = res['gs']
-                lat = res['lat']
-                lon = res['lon']
-                tr = res['tr']
-                vs = res['vs']
+        for serie in series:
+            (name, tags) = serie.split(',')
+            (tagName, value) = tags.split('=')
 
-                insert = f"pos,addr={addr} alt={alt},gs={gs},lat={lat},lon={lon},tr={tr},vs={vs} {ts}000000000"
-                # print("insert:", insert)
+            q = f"select * from {name} where {tagName}='{value}'"
+            rs = influx.client.query(query=q)
+            for r in rs:
+                # print('Num records in result set:', len(r))
+                for res in r:
+                    ts = int(parser.parse(res['time']).timestamp())
+                    addr = res['addr']
+                    alt = res['alt']
+                    gs = res['gs']
+                    lat = res['lat']
+                    lon = res['lon']
+                    tr = res['tr']
+                    vs = res['vs']
 
-                f.write(insert)
-                f.write('\n')
+                    insert = f"pos,addr={addr} alt={alt},gs={gs},lat={lat},lon={lon},tr={tr},vs={vs} {ts}000000000"
+                    # print("insert:", insert)
+
+                    f.write(insert)
+                    f.write('\n')
 
     influx.client.close()
 
