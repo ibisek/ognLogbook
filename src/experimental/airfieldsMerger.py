9a3ff1a43d8b4d843cb09356ea35d932c541e914
ibisek <ibisek@protonmail.com>
2020-07-23 08:44:09 +0200

    amended OSM file merging

--- src/experimental/airfieldsMerger.py
+++ src/experimental/airfieldsMerger.py
@@ -3,21 +3,53 @@
 # OSM-extracted location information CSV files.
 #
 
+import os
 import json
 
 AIRFIELDS_FN = '../../data/airfields.json'
 AIRFIELDS_FN_new = '../../data/airfields.json.new'
 
-OSM = '/home/jaja/btsync/doma/ogn/00-letiste/airfields-europe.csv'
-# OSM = '/home/jaja/btsync/doma/ogn/00-letiste/airfields-africa.csv'
-# OSM = '/home/jaja/btsync/doma/ogn/00-letiste/airfields-oceania.csv'
-# OSM = '/home/jaja/btsync/doma/ogn/00-letiste/airfields-camerica.csv'
-# OSM = '/home/jaja/btsync/doma/ogn/00-letiste/airfields-namerica.csv'
-# OSM = '/home/jaja/btsync/doma/ogn/00-letiste/airfields-samerica.csv'
+OSM_ROOT_DIR = '/home/jaja/btsync/doma/ogn/00-letiste/'
+
+new = 0
+old = 0
+
+
+def addToAfs(filepath, afs):
+    new = 0
+    old = 0
+
+    print(f'[INFO] Reading {filepath}')
+    with open(filepath, 'r') as f:
+        for line in f:
+            items = line.strip().split(';')
+
+            code = None
+            lat = 0
+            lon = 0
+
+            if len(items) == 3:
+                (code, lat, lon) = items
+            elif len(items) == 4:   # australian speciality
+                (code, code2, lat, lon) = items
+
+            lat = float(lat)
+            lon = float(lon)
+
+            if (code not in afs) and (4 <= len(code) <= 6):
+                afs[code] = {'lat': lat, 'lon': lon}
+                new += 1
+            else:
+                # print(f'{code} already in list')
+                old += 1
+
+        print(f'[INFO] New: {new}, old: {old}')
+
 
 if __name__ == '__main__':
 
     afs = dict()
+
     with open(AIRFIELDS_FN, 'r') as f:
         l = json.load(f)
         for item in l:
@@ -26,22 +58,11 @@ if __name__ == '__main__':
             lon = float(item.get('lon', 0))
             afs[code] = {'lat': lat, 'lon': lon}
 
-    new = 0
-    old = 0
-    with open(OSM, 'r') as f:
-        for line in f:
-            (code, lat, lon) = line.strip().split(';')
-            lat = float(lat)
-            lon = float(lon)
-
-            if code not in afs and 4 <= len(code) <= 6:
-                afs[code] = {'lat': lat, 'lon': lon}
-                new += 1
-            else:
-                # print(f'{code} already in list')
-                old += 1
-
-    print(f'New: {new}, old: {old}')
+    files = os.listdir(OSM_ROOT_DIR)
+    for filename in files:
+        if filename.endswith('.csv'):
+            filepath = f"{OSM_ROOT_DIR}/{filename}"
+            addToAfs(filepath, afs)
 
     with open(AIRFIELDS_FN_new, 'w') as f:
         l = list()
