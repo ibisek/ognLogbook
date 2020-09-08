445cac27c8700e220c565c88b0d330e849da1286
ibisek <panotrix@gmail.com>
2020-09-08 13:46:45 +0200

    -transaction

--- src/beaconProcessor.py
+++ src/beaconProcessor.py
@@ -211,11 +211,11 @@ class RawWorker(Thread):
 
             icaoLocation = f"'{icaoLocation}'" if icaoLocation else 'null'
 
-            strSql = f"START TRANSACTION; INSERT INTO logbook_events " \
+            strSql = f"INSERT INTO logbook_events " \
                 f"(ts, address, address_type, aircraft_type, event, lat, lon, location_icao, flight_time) " \
                 f"VALUES " \
                 f"({ts}, '{address}', {addressType}, '{aircraftType}', " \
-                f"'{event}', {lat:.5f}, {lon:.5f}, {icaoLocation}, {flightTime});COMMIT;"
+                f"'{event}', {lat:.5f}, {lon:.5f}, {icaoLocation}, {flightTime});"
 
             # print('strSql:', strSql)
 
