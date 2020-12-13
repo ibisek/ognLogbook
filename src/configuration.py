7ed7a0e2aeb4db11b0f5d15625a03525a1278426
ibisek <ibisek@protonmail.com>
2020-12-13 11:03:26 +0100

    +AIRFIELDS_FILE absolute path

--- src/configuration.py
+++ src/configuration.py
@@ -4,6 +4,7 @@ import os
 # APRS_FILTER = 'r/+49.3678/+16.1145/1100'    # 1100 km ~ eastern Romania
 APRS_FILTER = None
 
+AIRFIELDS_FILE = '/home/jaja/wqz/prog/python/ognLogbook/data/airfields.json'
 GEOFILE_PATH = '/home/jaja/data/download/ognLogbook/500m/mosaic-500m.TIF'
 
 redisConfig = {"host": "127.0.0.1", "password": "", "port": 6379}
