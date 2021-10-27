53fbb207419b663501b315640ba75efa54f77dc1
ibisek <ibisek@protonmail.com>
2021-10-27 22:55:55 +0200

    temporarily disabled ICAO filter to show all flights to everyone on the index page

--- index.py
+++ index.py
@@ -37,20 +37,20 @@ afCountryCodes = airfieldManager.afCountryCodes
 
 @app.route('/')
 def index():
-    langs = [al[0].lower() for al in request.accept_languages]
+    # langs = [al[0].lower() for al in request.accept_languages]
     icaoFilter = []
-    if 'cz' in langs or 'cs' in langs or 'sk' in langs:
-        icaoFilter.append('LK')
-        icaoFilter.append('LZ')
-    elif 'de' in langs:
-        icaoFilter.append('LO')     # at
-        icaoFilter.append('ED')     # de
-        icaoFilter.append('LS')     # ch
-    elif 'pl' in langs:
-        icaoFilter.append('EP')
-    if 'fi' in langs or 'se' in langs or 'no' in langs:
-        icaoFilter.append('EF')     # fi
-        icaoFilter.append('ES')     # se
+    # if 'cz' in langs or 'cs' in langs or 'sk' in langs:
+    #     icaoFilter.append('LK')
+    #     icaoFilter.append('LZ')
+    # elif 'de' in langs:
+    #     icaoFilter.append('LO')     # at
+    #     icaoFilter.append('ED')     # de
+    #     icaoFilter.append('LS')     # ch
+    # elif 'pl' in langs:
+    #     icaoFilter.append('EP')
+    # if 'fi' in langs or 'se' in langs or 'no' in langs:
+    #     icaoFilter.append('EF')     # fi
+    #     icaoFilter.append('ES')     # se
 
     departures, arrivals, flights = _prepareData(limit=25, icaoFilter=icaoFilter, sortTsDesc=True, orderByCol='landing_ts')
 
