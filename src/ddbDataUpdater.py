256bc97c9f4a0a7e8f07bff191cc063e3f39451a
ibisek <ibisek@protonmail.com>
2022-01-05 23:07:54 +0100

    FLARMNET work in progress

--- src/ddbDataUpdater.py
+++ src/ddbDataUpdater.py
@@ -20,7 +20,11 @@ FLARMNET_URL = 'https://www.flarmnet.org/static/files/wfn/data.fln'
 #     with DbSource(dbConnectionInfo).getConnection() as cur:
 #         cur.execute("DELETE FROM ddb;")
 
-def _insertOrAmendRecord(cursor: Cursor, deviceType: str, deviceId: str, aircraftType: str, registration: str, cn: str, tracked: bool = True, identified: bool = True):
+def _insertOrAmendRecord(cursor: Cursor,
+                         deviceType: str, deviceId: str,
+                         aircraftType: str, registration: str, cn: str,
+                         tracked: bool = True, identified: bool = True,
+                         updateRecordIfExists: bool = True):
     # MYSQL:
     # strSql = f"INSERT INTO ddb " \
     #          f"(device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified)" \
@@ -43,7 +47,7 @@ def _insertOrAmendRecord(cursor: Cursor, deviceType: str, deviceId: str, aircraf
     select = f"SELECT id, device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified FROM ddb WHERE device_id='{deviceId}' limit 1;"
 
     res = cursor.execute(select)
-    if res:
+    if res and updateRecordIfExists:
         (id1, devType1, devId1, aircraftType1, registration1, cn1, tracked1,
          identified1) = cursor.fetchone()  # (id, 'F', '000000', 'HPH 304CZ-17', 'OK-7777', 'KN', 1, 1)
         if devType1 != deviceType or aircraftType1 != aircraftType or registration1 != registration1 or cn1 != cn or tracked1 != tracked or identified1 != identified:
@@ -52,7 +56,7 @@ def _insertOrAmendRecord(cursor: Cursor, deviceType: str, deviceId: str, aircraf
                      f"WHERE id = {id1};"
             cursor.execute(update)
 
-    else:
+    elif not res:
         insert = f"INSERT INTO ddb " \
                  f"(device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified)" \
                  f"VALUES " \
@@ -62,16 +66,19 @@ def _insertOrAmendRecord(cursor: Cursor, deviceType: str, deviceId: str, aircraf
         cursor.execute(insert)
 
 
-def _processDDB():
-    print(f"Downloading data from {DDB_URL}")
-    resp = requests.get(DDB_URL)
+def _downloadDataFile(url: str):
+    print(f"Downloading data from {url}")
+    resp = requests.get(url)
 
     if resp.status_code != 200:
         print(f"[WARN] Download of the datafile failed with code {resp.status_code}")
         sys.exit(1)
 
-    lines = resp.text.split('\n')
+    return resp.text.split('\n')
 
+
+def _processDDB():
+    lines = _downloadDataFile(DDB_URL)
     if len(lines) < 10000:
         sys.exit(1)
 
@@ -93,17 +100,54 @@ def _processDDB():
             tracked = True if items[keys.index('TRACKED')] == 'Y' else False
             identified = True if items[keys.index('IDENTIFIED')] == 'Y' else False
 
-            _insertOrAmendRecord(cursor=cur,
+            _insertOrAmendRecord(cursor=cur, doUpdate=True,
                                  deviceType=deviceType, deviceId=deviceId,
                                  aircraftType=aircraftType, registration=registration, cn=cn,
                                  tracked=tracked, identified=identified)
 
 
 def _processFlarmnet():
-    # TODO
-    pass
+    # TODO get it from file first..
+
+    # lines = _downloadDataFile(FLARMNET_URL)
+    # if len(lines) < 10000:
+    #     sys.exit(1)
+
+    with DbSource(dbConnectionInfo).getConnection() as cur:
+
+        # TODO some parsing & stuff
+        # line = 'DD99C3;D-2265;Kestrel;D-2265;mc;123.675'
+        # items = line.split(';')
+
+        # 444446453831202020202020202020202020202020202020202020  -> devId 0-54 0-27    byte 0-27
+        # 442d36393138202020202020202020202020202020  -> registration 0-42      0-21 	byte 28-48
+        # 4c532d312066202020202020202020202020202020  -> aicraftType 0-42       0-21    byte 49-69
+        # 442d3639313820  -> reg #2?? 0-14                                      0-7     byte 70-76
+        # 465720  -> CN 0-6                                                     0-3     byte 77-79
+        # 3132332e353030 -> freq 0-14                                           0-7     byte 80-86
+
+        line = '444446453831202020202020202020202020202020202020202020442d363931382020202020202020202020202020204c532d312066202020202020202020202020202020442d36393138204657203132332e353030'
+        s = ""
+        for i in range(0, len(line), 2):
+            s += chr(int(line[i:i+2], 16))
+
+        deviceType = 'F'
+        deviceId = s[0:27].strip()  # xxyyzz
+        registration = s[27:48].strip()
+        aircraftType = s[48:69].strip()
+        cn = s[76:79].strip()
+        tracked = True
+        identified = True
+
+        print(f"XXX: {deviceId}, {registration}, {aircraftType}, {cn}")
+
+        # _insertOrAmendRecord(cursor=cur,
+        #                      deviceType=deviceType, deviceId=deviceId,
+        #                      aircraftType=aircraftType, registration=registration, cn=cn,
+        #                      tracked=tracked, identified=identified,
+        #                      doUpdate=False)
 
 
 if __name__ == '__main__':
     _processDDB()
-    _processFlarmnet()
+    # _processFlarmnet()
