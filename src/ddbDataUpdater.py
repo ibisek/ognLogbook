fdcdf64da0f8e91c0ff935ca5517c62ac41ac676
ibisek <ibisek@protonmail.com>
2022-01-05 22:28:55 +0100

    preparation for FLARMNET data processing

--- src/ddbDataUpdater.py
+++ src/ddbDataUpdater.py
@@ -10,17 +10,59 @@ import requests
 
 from configuration import dbConnectionInfo
 from db.DbSource import DbSource
+from pymysql.cursors import Cursor
 
 DDB_URL = 'https://ddb.glidernet.org/download/'
+FLARMNET_URL = 'https://www.flarmnet.org/static/files/wfn/data.fln'
 
 
-def _dropAllDdbRecords():
-    with DbSource(dbConnectionInfo).getConnection() as cur:
-        cur.execute("DELETE FROM ddb;")
+# def _dropAllDdbRecords():
+#     with DbSource(dbConnectionInfo).getConnection() as cur:
+#         cur.execute("DELETE FROM ddb;")
+
+def _insertOrAmendRecord(cursor: Cursor, deviceType: str, deviceId: str, aircraftType: str, registration: str, cn: str, tracked: bool = True, identified: bool = True):
+    # MYSQL:
+    # strSql = f"INSERT INTO ddb " \
+    #          f"(device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified)" \
+    #          f"VALUES " \
+    #          f"(%(device_type)s, %(device_id)s, %(aircraft_type)s, %(aircraft_registration)s, %(aircraft_cn)s, %(tracked)s, %(identified)s);"
+    #
+    # data = dict()
+    # data['device_type'] = deviceType
+    # data['device_id'] = deviceId
+    # data['aircraft_type'] = aircraftType
+    # data['aircraft_registration'] = registration
+    # data['aircraft_cn'] = cn
+    # data['tracked'] = tracked
+    # data['identified'] = identified
+    #
+    # with DbSource(dbConnectionInfo).getConnection() as cur:
+    #     cur.execute(strSql, data)
+
+    # SQLITE:
+    select = f"SELECT id, device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified FROM ddb WHERE device_id='{deviceId}' limit 1;"
+
+    res = cursor.execute(select)
+    if res:
+        (id1, devType1, devId1, aircraftType1, registration1, cn1, tracked1,
+         identified1) = cursor.fetchone()  # (id, 'F', '000000', 'HPH 304CZ-17', 'OK-7777', 'KN', 1, 1)
+        if devType1 != deviceType or aircraftType1 != aircraftType or registration1 != registration1 or cn1 != cn or tracked1 != tracked or identified1 != identified:
+            update = f"UPDATE ddb SET device_type = '{deviceType}', aircraft_type='{aircraftType}', aircraft_registration='{registration}', " \
+                     f"aircraft_cn='{cn}', tracked={tracked}, identified={identified} " \
+                     f"WHERE id = {id1};"
+            cursor.execute(update)
+
+    else:
+        insert = f"INSERT INTO ddb " \
+                 f"(device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified)" \
+                 f"VALUES " \
+                 f"('{deviceType}', '{deviceId}', '{aircraftType}', '{registration}', '{cn}', " \
+                 f"{tracked}, {identified});"
+
+        cursor.execute(insert)
 
 
-if __name__ == '__main__':
-
+def _processDDB():
     print(f"Downloading data from {DDB_URL}")
     resp = requests.get(DDB_URL)
 
@@ -51,43 +93,17 @@ if __name__ == '__main__':
             tracked = True if items[keys.index('TRACKED')] == 'Y' else False
             identified = True if items[keys.index('IDENTIFIED')] == 'Y' else False
 
-            # MYSQL:
-            # strSql = f"INSERT INTO ddb " \
-            #          f"(device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified)" \
-            #          f"VALUES " \
-            #          f"(%(device_type)s, %(device_id)s, %(aircraft_type)s, %(aircraft_registration)s, %(aircraft_cn)s, %(tracked)s, %(identified)s);"
-            #
-            # data = dict()
-            # data['device_type'] = deviceType
-            # data['device_id'] = deviceId
-            # data['aircraft_type'] = aircraftType
-            # data['aircraft_registration'] = registration
-            # data['aircraft_cn'] = cn
-            # data['tracked'] = tracked
-            # data['identified'] = identified
-            #
-            # with DbSource(dbConnectionInfo).getConnection() as cur:
-            #     cur.execute(strSql, data)
-
-            # SQLITE:
-            select = f"SELECT id, device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified FROM ddb WHERE device_id='{deviceId}' limit 1;"
-
-            res = cur.execute(select)
-            if res:
-                (id1, devType1, devId1, aircraftType1, registration1, cn1, tracked1, identified1) = cur.fetchone()    # (id, 'F', '000000', 'HPH 304CZ-17', 'OK-7777', 'KN', 1, 1)
-                if devType1 != deviceType or aircraftType1 != aircraftType or registration1 != registration1 or cn1 != cn or tracked1 != tracked or identified1 != identified:
-                    update = f"UPDATE ddb SET device_type = '{deviceType}', aircraft_type='{aircraftType}', aircraft_registration='{registration}', " \
-                             f"aircraft_cn='{cn}', tracked={tracked}, identified={identified} " \
-                             f"WHERE id = {id1};"
-                    cur.execute(update)
-
-            else:
-                insert = f"INSERT INTO ddb " \
-                         f"(device_type, device_id, aircraft_type, aircraft_registration, aircraft_cn, tracked, identified)" \
-                         f"VALUES " \
-                         f"('{deviceType}', '{deviceId}', '{aircraftType}', '{registration}', '{cn}', " \
-                         f"{tracked}, {identified});"
-
-                cur.execute(insert)
+            _insertOrAmendRecord(cursor=cur,
+                                 deviceType=deviceType, deviceId=deviceId,
+                                 aircraftType=aircraftType, registration=registration, cn=cn,
+                                 tracked=tracked, identified=identified)
 
 
+def _processFlarmnet():
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    _processDDB()
+    _processFlarmnet()
