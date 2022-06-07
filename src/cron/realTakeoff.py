53a881020b34b2e390360cb03d810a929066678e
ibisek <ibisek@protonmail.com>
2022-06-07 14:38:26 +0200

    look-up takeoff location if not already set

--- src/cron/realTakeoff.py
+++ src/cron/realTakeoff.py
@@ -4,14 +4,14 @@ data filtering. This cronjob is to look up the true take-off location and ts
 by following the path back in time.
 """
 
-from queue import Queue, Empty
 from datetime import datetime
 from typing import List
 
+from airfieldManager import AirfieldManager
 from configuration import dbConnectionInfo, INFLUX_DB_HOST, INFLUX_DB_NAME
+from dataStructures import LogbookItem, addressPrefixes
 from db.DbSource import DbSource
 from db.InfluxDbThread import InfluxDbThread
-from dataStructures import LogbookItem, addressPrefixes
 from utils import getGroundSpeedThreshold
 
 
@@ -19,16 +19,16 @@ class RealTakeoffLookup(object):
     RUN_INTERVAL = 60  # [s]
 
     def __init__(self):
-        self.queue = Queue()
         print(f"[INFO] RealTakeoffLookup scheduled to run every {self.RUN_INTERVAL}s.")
 
         self.influxDb = InfluxDbThread(dbName=INFLUX_DB_NAME, host=INFLUX_DB_HOST, startThread=False)
+        self.airfieldManager = AirfieldManager()
 
     def __del__(self):
         self.influxDb.client.close()
 
     def _listTakeoffs(self, ts) -> List[LogbookItem]:
-        strSql = f"SELECT id, ts, address, address_type FROM logbook_events " \
+        strSql = f"SELECT id, ts, address, address_type, location_icao FROM logbook_events " \
                  f"WHERE ts >= {ts - self.RUN_INTERVAL} AND event='T';"
 
         l = []
@@ -41,8 +41,8 @@ class RealTakeoffLookup(object):
                 if not row:
                     break
 
-                id, ts, address, address_type = row
-                item = LogbookItem(id=id, takeoff_ts=ts, address=address, address_type=address_type)
+                id, ts, address, address_type, location_icao = row
+                item = LogbookItem(id=id, takeoff_ts=ts, address=address, address_type=address_type, takeoff_icao=location_icao)
                 l.append(item)
 
         return l
@@ -76,10 +76,14 @@ class RealTakeoffLookup(object):
                         break
 
                 if dirty:
+                    if not logbookItem.takeoff_icao:
+                        logbookItem.takeoff_icao = self.airfieldManager.getNearest(logbookItem.takeoff_lat, logbookItem.takeoff_lon)
+
                     logbookItem.takeoff_ts = int(datetime.strptime(logbookItem.takeoff_ts, '%Y-%m-%dT%H:%M:%SZ').timestamp())
 
                     updateSql = f"UPDATE logbook_events SET ts={logbookItem.takeoff_ts}, " \
-                                f"lat={logbookItem.takeoff_lat}, lon={logbookItem.takeoff_lon} " \
+                                f"lat={logbookItem.takeoff_lat}, lon={logbookItem.takeoff_lon}, " \
+                                f"location_icao='{logbookItem.takeoff_icao}' " \
                                 f"WHERE id={logbookItem.id};"
 
                     with DbSource(dbConnectionInfo).getConnection().cursor() as cur:
