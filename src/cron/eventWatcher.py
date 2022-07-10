ade3e69e241493b78b5e05491e4b9632b312190f
ibisek <ibisek@protonmail.com>
2022-07-10 13:17:11 +0200

    EventWatcher wip

--- src/cron/eventWatcher.py
+++ src/cron/eventWatcher.py
@@ -2,10 +2,33 @@
 Event Watcher to inform retistered users about airplane events (take-off/landing)
 """
 
+from datetime import datetime
 from redis import StrictRedis
 
-from configuration import dbConnectionInfo, redisConfig, ADDRESS_TYPE_PREFIX
+from configuration import dbConnectionInfo, redisConfig, ADDRESS_TYPE_PREFIX, ADDRESS_TYPES
 from db.DbSource import DbSource
+
+
+class WatcherEvent:
+    def __init__(self, line):
+        self.ts, self.event, self.address, self.addressType, self.lat, self.lon, self.icaoLocation, self.flightTime = line.decode('utf-8').split(';')
+
+        self.ts = int(self.ts)
+        self.addressType = int(self.addressType)
+        self.lat = float(self.lat)
+        self.lon = float(self.lon)
+        self.flightTime = int(self.flightTime) if self.flightTime else 0
+
+    @property
+    def addressWithPrefix(self):
+        addrPrefix = ADDRESS_TYPE_PREFIX[int(self.addressType)]
+        return f"{addrPrefix}{self.address}"
+
+
+class Watcher:
+    def __init__(self, row):
+        self.userId, self.email, self.lang, self.aircraft_registration, self.aircraft_cn = row
+        self.userId = int(self.userId)
 
 
 class EventWatcher:
@@ -21,22 +44,27 @@ class EventWatcher:
         rec = f"{ts};{event};{address};{addressType};{lat:.5f};{lon:.5f};{icaoLocation};{flightTime}"
         redis.rpush(EventWatcher.REDIS_KEY, rec)
 
-    def _listWatchers(self, addressWithPrefix):
-        strSql = f"SELECT * FROM watchers WHERE address = '{addressWithPrefix}';"
-        print("strSql:", strSql)
+    def _listWatchers(self, addressType: int, address: str):
+        addressTypeChar: str = ADDRESS_TYPES[addressType]
+        strSql = f"SELECT u.id, u.email, u.lang, d.aircraft_registration, d.aircraft_cn FROM watchers AS w " \
+                 f"LEFT JOIN users AS u ON u.id = w.user_id " \
+                 f"LEFT JOIN ddb AS d ON d.device_id = w.addr AND d.device_type = w.addr_type " \
+                 f"WHERE addr_type = '{addressTypeChar}' AND addr = '{address}';"
 
+        watchers = []
         with DbSource(dbConnectionInfo).getConnection().cursor() as cur:
             cur.execute(strSql)
             for row in cur:
-                print("row:", row)
+                w = Watcher(row)
+                watchers.append(w)
 
-        # TODO
+        return watchers
 
-        return ['dummy watcher instance']
+    def _notifyWatcher(self, watcher: Watcher, event: WatcherEvent):
+        if event.icaoLocation:
+            print(f"[INFO] WATCHER [{event.ts}] <{event.event}> @ {event.icaoLocation} {watcher.aircraft_registration} ({watcher.aircraft_cn})")
 
-    def _notifyWatcher(self, watcher):
-        print(f"Notifying '{watcher}'..")
-        # TODO
+        # TODO poslat mail
 
     def processEvents(self):
         numRecs = self.redis.llen(EventWatcher.REDIS_KEY)
@@ -44,19 +72,17 @@ class EventWatcher:
             return
 
         while rec := self.redis.lpop(EventWatcher.REDIS_KEY):
-            ts, event, address, addressType, lat, lon, icaoLocation, flightTime = rec.decode('utf-8').split(';')
-            addrPrefix = ADDRESS_TYPE_PREFIX[int(addressType)]
-
-            watchers = self._listWatchers(f"{addrPrefix}{address}")
+            event = WatcherEvent(rec)
+            watchers = self._listWatchers(event.addressType, event.address)
 
             for watcher in watchers:
-                self._notifyWatcher(watcher)
+                self._notifyWatcher(watcher, event)
 
 
 if __name__ == '__main__':
     redis = StrictRedis(**redisConfig)
     EventWatcher.createEvent(redis=redis,
-                             ts=1, event='X', address='123456', addressType=0,
+                             ts=int(datetime.now().timestamp()), event='?', address='C35001', addressType=3,
                              lat=49.123, lon=16.123, icaoLocation='LKKA', flightTime=666)
 
     ev = EventWatcher()
