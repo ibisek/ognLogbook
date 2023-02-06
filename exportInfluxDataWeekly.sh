a7085e39895aed14fee48b6d356e4349e525c707
ibisek <ibisek@protonmail.com>
2023-02-06 11:31:51 +0100

    config through env vars

--- exportInfluxDataWeekly.sh
+++ exportInfluxDataWeekly.sh
@@ -8,6 +8,10 @@ source ./venv/bin/activate
 
 export PYTHONPATH=.:./src:$PYTHONPATH
 
+export INFLUX_DB_NAME='ogn_logbook'
+#export INFLUX_DB_NAME='ogn_logbook_stm'
+export STORAGE_DIR='/media/data/archive'
+
 python3 ./src/experimental/influx_exportData_weekly.py
 
 deactivate
