4c757ed574e72f743ed05453b70212b9b3fdfb40
ibisek <ibisek@protonmail.com>
2023-08-25 17:11:38 +0200

    WIP: added lookup for flights that are to be added on the map

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,5 @@
+var searchInProgress = false;
+var searchResults = document.getElementById("searchResults");
 
 function onPageLoad() {
     var map = L.map('map'); //.setView([51.505, -0.09], 13);
@@ -46,4 +48,85 @@ function onPageLoad() {
     var landingMarker = L.marker(lastFlightSegment[lastFlightSegment.length-1],{icon: redIcon}).addTo(map);
     landingMarker.bindPopup("<b>Landing</b>");
 
+}
+
+function addButtonClicked() {
+    document.getElementById("addButton").style.visibility = 'hidden';
+    document.getElementById("sideBar").style.visibility = 'visible';
+}
+
+function closeSideBar() {
+    document.getElementById("sideBar").style.visibility = 'hidden';
+    document.getElementById("addButton").style.visibility = 'visible';
+}
+
+function searchFlight() {
+    var date = document.getElementById("date").value.trim();
+    var loc = document.getElementById("location").value.trim();
+    var reg = document.getElementById("reg").value.trim();
+    var cn = document.getElementById("cn").value.trim();
+
+    if(!loc && !reg && !cn) {
+        alert("WTF?!");
+        return;
+    }
+
+    if(searchInProgress) {
+        alert("Search already in progress!");
+        return;
+    }
+
+    searchStarted();
+
+    var query = `/ff?date=${date}&loc=${loc}&reg=${reg}&cn=${cn}`;
+
+    const req = new XMLHttpRequest();
+    req.open("GET", query, true);
+    req.onload = onFligthSearchResp;
+    req.onerror = (e) => {
+      console.error(req.statusText);
+      alert('nejaky problem ve vyhledavani letu #2');
+    };
+    req.send(null);
+}
+
+function searchStarted() {
+    searchInProgress = true;
+    document.getElementById("sideBarSearchBtn").disabled=true;
+    document.getElementById("labelForSearchResults").style.visibility='hidden';
+    searchResults.textContent = "Working..";
+}
+
+function searchFinished() {
+    searchInProgress = false;
+    document.getElementById("sideBarSearchBtn").disabled=false;
+    document.getElementById("labelForSearchResults").style.visibility='visible';
+    searchResults.innerHtml = "";
+}
+
+function onFligthSearchResp(e) {
+    searchFinished();
+
+    //var req = e.originalTarget;
+    var resp = e.currentTarget;
+    if (resp.readyState === 4 && resp.status === 200) {
+        var content = "<table>";
+        var flights = JSON.parse(resp.responseText);
+        for (const flight of flights) {
+            var reg = flight.reg ? flight.reg : '?';
+            var cn = flight.cn ? flight.cn : '?';
+            var takeoffLoc = flight.to_loc ? flight.to_loc : 'UNK';
+            //var to_ts = new Date(flight.to_ts*1000);
+            var landingLoc = flight.to_loc ? flight.la_loc : 'UNK';
+            //var la_ts = new Date(flight.la_ts*1000);
+
+            content += `<tr><td>${reg} (${cn})</td><td>${takeoffLoc} &#8605; ${landingLoc}<td><td><a href="${flight.id}">(+)</a></td></tr>`
+        }
+        content += "</table>"
+
+        searchResults.innerHTML = content;
+
+    } else {
+        alert('nejaky problem ve vyhledavani letu #1');
+    }
 }
