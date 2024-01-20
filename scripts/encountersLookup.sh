868dee0020d0e96c0c0d06e34f7dc37b78af437a
ibisek <ibisek@protonmail.com>
2024-01-20 20:55:47 +0100

    unbuffered output

--- scripts/encountersLookup.sh
+++ scripts/encountersLookup.sh
@@ -4,7 +4,7 @@ cd ..
 source ./venv/bin/activate
 export PYTHONPATH=./src:$PYTHONPATH
 
-python3 ./src/cron/encountersLookup.py |tee -a ../encounters.log
+python3 -u ./src/cron/encountersLookup.py |tee -a ../encounters.log
 
 cd -
 deactivate
