ca85bade0d3165c03c2cfa3153f5669fb91ab481
ibisek <ibisek@protonmail.com>
2024-10-16 21:53:00 +0200

    cache values in redis for faster index.py response

--- src/dao/stats.py
+++ src/dao/stats.py
@@ -1,89 +1,112 @@
 
 from datetime import datetime
 
-from configuration import dbConnectionInfo
+from redis import StrictRedis
+
+from configuration import dbConnectionInfo, redisConfig
 from db.DbSource import DbSource
 from utilsTime import formatDuration
 from utils import getDayTimestamps
 
 
-def getTotNumFlights():
-    num = 0
+class Stats:
 
-    try:
-        with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
-            sql = "SELECT count(address) FROM logbook_entries;"
-            res = c.execute(sql)
+    REDIS_KEY = 'TOT_NUM_OF_FLIGHTS'
 
-            if res:
-                num = c.fetchone()[0]
+    def __init__(self):
+        self.redis = self.redis = StrictRedis(**redisConfig)
 
-    except Exception as ex:
-        print('[ERROR] in stats #1:', str(ex))
+    @staticmethod
+    def _getTotNumFlightsFromDB():
+        num = 0
 
-    return num
+        try:
+            with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
+                sql = "SELECT count(address) FROM logbook_entries;"
+                res = c.execute(sql)
 
+                if res:
+                    num = c.fetchone()[0]
 
-def getNumFlightsToday():
-    num = 0
-    startTs, endTs = getDayTimestamps(datetime.now())
+        except Exception as ex:
+            print('[ERROR] in stats #1:', str(ex))
+            num = None
 
-    try:
-        with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
-            sql = f"SELECT count(address) FROM logbook_entries WHERE takeoff_ts >= {startTs} AND landing_ts <= {endTs};"
-            res = c.execute(sql)
+        return num
 
-            if res:
-                num = c.fetchone()[0]
+    def getTotNumFlights(self):
+        num = 0
 
-    except Exception as ex:
-        print('[ERROR] in stats #2:', str(ex))
+        if self.redis.exists(self.REDIS_KEY):
+            num = int(self.redis.get(self.REDIS_KEY))
 
-    return num
+        if num < 666:   # an arbitrary value .. num is already 10M+ (might be set to 1 in first INCR pass-thru in beaconProcessor).
+            num = self._getTotNumFlightsFromDB()
+            self.redis.set(self.REDIS_KEY, num)
 
+        return num
 
-def getLongestFlightToday():
-    retVal = (None, None)
-    startTs, endTs = getDayTimestamps(datetime.now())
+    @staticmethod
+    def getNumFlightsToday():
+        num = 0
+        startTs, endTs = getDayTimestamps(datetime.now())
 
-    try:
-        with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
-            sql = f"SELECT id, flight_time FROM logbook_entries " \
-                f"WHERE takeoff_ts >= {startTs} AND landing_ts <= {endTs} " \
-                f"ORDER BY flight_time DESC LIMIT 1;"
-            res = c.execute(sql)
+        try:
+            with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
+                sql = f"SELECT count(address) FROM logbook_entries WHERE takeoff_ts >= {startTs} AND landing_ts <= {endTs};"
+                res = c.execute(sql)
 
-            if res:
-                row = c.fetchone()
-                logbookEntryId = row[0]
-                seconds = row[1]
-                retVal = (logbookEntryId, formatDuration(seconds))
+                if res:
+                    num = c.fetchone()[0]
 
-    except Exception as ex:
-        print('[ERROR] in stats #3:', str(ex))
+        except Exception as ex:
+            print('[ERROR] in stats #2:', str(ex))
 
-    return retVal
+        return num
 
+    @staticmethod
+    def getLongestFlightToday():
+        retVal = (None, None)
+        startTs, endTs = getDayTimestamps(datetime.now())
 
-def getHighestTrafficToday():
-    retVal = None, 0
-    startTs, endTs = getDayTimestamps(datetime.now())
+        try:
+            with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
+                sql = f"SELECT id, flight_time FROM logbook_entries " \
+                    f"WHERE takeoff_ts >= {startTs} AND landing_ts <= {endTs} " \
+                    f"ORDER BY flight_time DESC LIMIT 1;"
+                res = c.execute(sql)
 
-    try:
-        with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
-            sql = f"SELECT location_icao, count(address) AS n FROM logbook_events " \
-                f"WHERE ts >= {startTs} AND ts <= {endTs} " \
-                f"AND location_icao is not null " \
-                f"AND location_icao != 'None' " \
-                f"GROUP BY location_icao ORDER BY n DESC LIMIT 1;"
-            # .. f"AND location_icao like 'LK%'" \
-            res = c.execute(sql)
+                if res:
+                    row = c.fetchone()
+                    logbookEntryId = row[0]
+                    seconds = row[1]
+                    retVal = (logbookEntryId, formatDuration(seconds))
 
-            if res:
-                row = c.fetchone()
-                retVal = row[0], row[1]
-    except Exception as ex:
-        print('[ERROR] in stats #4:', str(ex))
+        except Exception as ex:
+            print('[ERROR] in stats #3:', str(ex))
 
-    return retVal
+        return retVal
+
+    @staticmethod
+    def getHighestTrafficToday():
+        retVal = None, 0
+        startTs, endTs = getDayTimestamps(datetime.now())
+
+        try:
+            with DbSource(dbConnectionInfo=dbConnectionInfo).getConnection().cursor() as c:
+                sql = f"SELECT location_icao, count(address) AS n FROM logbook_events " \
+                    f"WHERE ts >= {startTs} AND ts <= {endTs} " \
+                    f"AND location_icao is not null " \
+                    f"AND location_icao != 'None' " \
+                    f"GROUP BY location_icao ORDER BY n DESC LIMIT 1;"
+                # .. f"AND location_icao like 'LK%'" \
+                res = c.execute(sql)
+
+                if res:
+                    row = c.fetchone()
+                    retVal = row[0], row[1]
+        except Exception as ex:
+            print('[ERROR] in stats #4:', str(ex))
+
+        return retVal
 
