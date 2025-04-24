e9ded82944dfbcb8b26379fe36a76563d35d3510
ibisek <ibisek@protonmail.com>
2025-04-24 12:50:15 +0200

    return date, not datetime

--- src/utilsTime.py
+++ src/utilsTime.py
@@ -41,6 +41,6 @@ def getLocalTzDate(utcTs: int, lat: float, lon: float) -> str:
 
     except pytz.exceptions.UnknownTimeZoneError as e:
         print(f"[ERROR] Unknown timezone for lat {lat:.4f} lon {lon:.4f}:", str(e))
-        dateLocal = datetime.fromtimestamp(utcTs, tz=pytz.timezone('UTC'))
+        dateLocal = datetime.fromtimestamp(utcTs, tz=pytz.timezone('UTC')).strftime('%Y-%m-%d')
 
     return dateLocal
