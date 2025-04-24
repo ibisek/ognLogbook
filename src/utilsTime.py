a73efb5a0dd3e04089fbbb3cd6615d3c1149a4fe
ibisek <ibisek@protonmail.com>
2025-04-24 12:09:16 +0200

    +getLocalTzDate()

--- src/utilsTime.py
+++ src/utilsTime.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+import pytz
+from tzfpy import get_tz
+
+
 def formatDuration(seconds):
     if not seconds:
         return 0
@@ -20,3 +25,21 @@ def formatDuration(seconds):
         dur = f"{m}'"
 
     return dur
+
+
+def getLocalTzDate(utcTs: int, lat: float, lon: float) -> str:
+    """
+    @return local date in given timezone by location
+    """
+    dateLocal = None
+
+    try:
+        tzStr = get_tz(lon, lat)  # !! order LON , LAT !!
+        tzInfo = pytz.timezone(tzStr)
+        dtLocal = datetime.fromtimestamp(utcTs, tz=tzInfo)
+        dateLocal = dtLocal.strftime('%Y-%m-%d')
+
+    except pytz.exceptions.UnknownTimeZoneError as e:
+        print(f"[ERROR] Unknown timezone for lat {lat:.4f} lon {lon:.4f}:", str(e))
+
+    return dateLocal
