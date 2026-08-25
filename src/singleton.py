05f435071422666580ba01077b469bcfde7aa9b9
ibisek <ibisek@protonmail.com>
2026-08-25 15:20:02 +0200

    +ThreadSafeSingleton

--- src/singleton.py
+++ src/singleton.py
@@ -1,4 +1,7 @@
 
+from threading import Lock
+
+
 class Singleton(type):
     _instances = {}
 
@@ -6,3 +9,15 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+class ThreadSafeSingleton:
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+        return cls._instance
