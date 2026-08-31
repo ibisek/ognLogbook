4957e677bf4f8e0670063e71e3022474d606a693
ibisek <ibisek@protonmail.com>
2026-08-31 12:13:07 +0200

    err fix

--- src/dao/logs.py
+++ src/dao/logs.py
@@ -19,7 +19,7 @@ def logIgcDownload(userId: str, recType: str, recId: int, remoteAddr: str):
     if recType not in ('f', 't'):
         return
 
-    if remoteAddr > 45:     # IPv6 / mapped IPv4 addresses might be up to 45 chars long
+    if len(remoteAddr) > 45:     # IPv6 / mapped IPv4 addresses might be up to 45 chars long
         log.warning(f"Excessively long remoteAddr: {remoteAddr}")
 
     ts = round(datetime.now(tz=timezone.utc).timestamp())
